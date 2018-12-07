7a13dfb580273077a498a8ee7d0c8c14cc03e90b
Artsiom Sinitski <artsiom.vs@gmail.com>
2018-12-07 19:19:37 +0300

    Added capped crowdsale functionality and tests

--- contracts/AvsTokenCrowdsale.sol
+++ contracts/AvsTokenCrowdsale.sol
@@ -2,12 +2,44 @@ pragma solidity ^0.4.24;
 
 import "zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "zeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 
-contract AvsTokenCrowdsale is Crowdsale, MintedCrowdsale {
+contract AvsTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale {
     //STATE DATA
+    //Track investor contribution amounts
+    uint256 public investorMinCap = 2000000000000000;
+    uint256 public investorMaxCap = 50000000000000000000;
+    mapping (address => uint256) public contributions;
+
 
     //CONSTRUCTOR
-    constructor (uint256 _rate, address _wallet, ERC20 _token)
-        Crowdsale(_rate, _wallet, _token) public {
+    constructor (uint256 _rate, address _wallet, ERC20 _token, uint256 _cap)
+        Crowdsale(_rate, _wallet, _token)
+        CappedCrowdsale(_cap) public {
+    }
+
+    // GETTERS & SETTERS
+    /*@dev Returns the amt contributed so far by a specific user
+     *@param Address of the contributor
+     *@return total user contributions up to this point in time
+     */
+    function getUserContribution(address _beneficiary) public view returns (uint256) {
+        return contributions[_beneficiary];
+    }
+
+    // CONTRACT METHODS
+    /*
+    * @dev Extend parent behavior requiring purchase to respect investor min/max funding cap.
+    * @param _beneficiary Token purchaser
+    * @param _weiAmount Amount of wei contributed
+    */
+    function _preValidatePurchase(
+     address _beneficiary,
+     uint256 _weiAmt) internal {
+         super._preValidatePurchase(_beneficiary, _weiAmt);
+         uint256 prevContribution = contributions[_beneficiary];
+         uint256 _newContribution = prevContribution  + _weiAmt;
+         require(_newContribution >= investorMinCap && _newContribution <= investorMaxCap);
+         contributions[_beneficiary] = _newContribution;
     }
 }
--- test/TestAvsTokenCrowdsale.js
+++ test/TestAvsTokenCrowdsale.js
@@ -1,6 +1,7 @@
 //import ether from './helpers/Ether';
 /* Node.js uses CommonJS module syntax, otherwise will get compiler errors*/
 const ether = require('./helpers/Ether');
+const EVMrevert = require('./helpers/EVMrevert');
 const BigNumber = web3.BigNumber;
 
 require('chai')
@@ -29,11 +30,17 @@ contract('AvsTokenCrowdsale', function([_, wallet, investor1, investor2]) {
     // Crowdsale config
     this.rate = 500;
     this.wallet = wallet;
+    this.cap = ether(100);
+
+    // Investor Min & Max caps
+    this.investorMinCap = ether(0.002);
+    this.investorMaxCap = ether(50);
 
     this.crowdsale = await AvsTokenCrowdsale.new(
       this.rate,
       this.wallet,
-      this.token.address
+      this.token.address,
+      this.cap
     );
 
    // Transfer token ownership to crowdsale
@@ -66,6 +73,52 @@ contract('AvsTokenCrowdsale', function([_, wallet, investor1, investor2]) {
     });
   });
 
+  describe('capped crowdsale', async function() {
+    it('has correct hard cap', async function() {
+      const cap = await this.crowdsale.cap();
+      cap.should.be.bignumber.equal(this.cap);
+    });
+  });
+
+  describe('buyTokens()', function() {
+    describe('if contribution is less than the min cap', function() {
+      it('rejects the transaction', async function() {
+        const value = this.investorMinCap - 1;
+        await this.crowdsale.buyTokens(investor2, {value: value, from: investor2}).should.be.rejectedWith(EVMrevert);
+      });
+    });
+
+    describe('when the investor has already met the min cap', function() {
+      it('allows investor contribute below the min cap', async function() {
+        const value1 = ether(1);
+        await this.crowdsale.buyTokens(investor1, {value: value1, from: investor1});
+        const value2 = 1; // 1 wei
+        await this.crowdsale.buyTokens(investor1, {value: value2, from: investor1}).should.be.fulfilled;
+      });
+    });
+    
+    describe('if the total contributions exceed the max cap', function() {
+      it('rejects the transaction', async function() {
+        //first contribution is in a valid range
+        const value = ether(2);
+        await this.crowdsale.buyTokens(investor1, {value: value, from: investor1});
+        //second contribution sends total contribution over the max cap
+        const value2 = ether(49);
+        await this.crowdsale.buyTokens(investor1, {value: value2, from: investor1}).should.be.rejectedWith(EVMrevert);
+      });
+    });
+
+    describe('if the contibution is within the allowed limits', function() {
+      it('succeeds and updates the contribution amount', async function() {
+          const value = ether(2);
+          await this.crowdsale.buyTokens(investor2, {value: value, from: investor2}).should.be.fulfilled;
+          const contribution = await this.crowdsale.getUserContribution(investor2);
+          contribution.should.be.bignumber.equal(value);
+      })
+    })
+
+  });
+
   describe('accepting payments', function() {
     it('should accept payments', async function() {
       const value = ether(1);
@@ -74,5 +127,5 @@ contract('AvsTokenCrowdsale', function([_, wallet, investor1, investor2]) {
       await this.crowdsale.buyTokens(investor1, { value: value, from: purchaser }).should.be.fulfilled;
     });
   });
-
 });
+
