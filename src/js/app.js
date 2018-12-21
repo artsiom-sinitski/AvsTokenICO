2401912296d45c498c6c8a0cad33526ce605b037
Artsiom Sinitski <artsiom.vs@gmail.com>
2018-12-21 12:28:33 +0000

    added more functions

--- src/js/app.js
+++ src/js/app.js
@@ -12,10 +12,12 @@ App = {
             // If a web3 instance is already provided by Meta Mask.
             App.web3Provider = web3.currentProvider;
             web3 = new Web3(web3.currentProvider);
+        console.log("IF branch: ", web3);
           } else {
             // Specify default instance if no web3 instance provided
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
             web3 = new Web3(App.web3Provider);
+        console.log("else branch: ", web3);
           }
           return App.initContracts();
     },
@@ -23,9 +25,22 @@ App = {
     initContracts: function() {
         $.getJSON("AvsTokenCrowdsale.json", function(avsTokenCrowdsale) {
             App.contracts.AvsTokenCrowdsale = TruffleContract(avsTokenCrowdsale);
+        console.log("avsTokenCrowd: ", avsTokenCrowdsale);
             App.contracts.AvsTokenCrowdsale.setProvider(App.web3Provider);
+        // web3.version.getNetwork(function(err, res) {console.log(res); });
             App.contracts.AvsTokenCrowdsale.deployed().then(function(avsTokenCrowdsale) {
                 console.log("Avs Token Crowdsale address:", avsTokenCrowdsale.address);
+            });
+        }).done(function() {
+            $.getJSON("AvsToken.json", function(avsToken) {
+              App.contracts.AvsToken = TruffleContract(avsToken);
+              App.contracts.AvsToken.setProvider(App.web3Provider);
+              App.contracts.AvsToken.deployed().then(function(avsToken) {
+                console.log("AvS Token Address:", avsToken.address);
+              });
+      
+              //App.listenForEvents();
+              //return App.render();
             });
         });
     }
